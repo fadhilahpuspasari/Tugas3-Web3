690912c6ae2b453af41b794bcf2e6e6556264c54
fadhilahpuspasari <fadhilahpuspasari.48@gmail.com>
2020-03-23 21:19:20 +0700

    Updates

---
+++ precache-manifest.ea2d4e4605fbbd56ddc64e91ab882753.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41f15800be80749eab99aa2b6200a81",
+    "url": "/Tugas3-Web3-Fadhilah/index.html"
+  },
+  {
+    "revision": "238f4eda8c3832d26b56",
+    "url": "/Tugas3-Web3-Fadhilah/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "b63a26fd0876d04db24b",
+    "url": "/Tugas3-Web3-Fadhilah/static/css/main.3f324957.chunk.css"
+  },
+  {
+    "revision": "238f4eda8c3832d26b56",
+    "url": "/Tugas3-Web3-Fadhilah/static/js/2.725ce827.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/Tugas3-Web3-Fadhilah/static/js/2.725ce827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63a26fd0876d04db24b",
+    "url": "/Tugas3-Web3-Fadhilah/static/js/main.b632cbc5.chunk.js"
+  },
+  {
+    "revision": "df51824305ed30426aaa",
+    "url": "/Tugas3-Web3-Fadhilah/static/js/runtime-main.0813c8ce.js"
+  },
+  {
+    "revision": "9bd64bb61172e23230409be3d1452522",
+    "url": "/Tugas3-Web3-Fadhilah/static/media/picture.9bd64bb6.svg"
+  }
+]);
